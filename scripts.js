07de524313219657d199ef4806962afc33d5e274
Nandon <nandonarts@gmail.com>
2022-11-02 20:51:17 -0400

    Finishing Touchesto Javascript

--- scripts.js
+++ scripts.js
@@ -4,19 +4,18 @@ window.addEventListener('DOMContentLoaded',(event) =>{
 
 
 
-let set = document.querySelector("input #email");
+let set = document.getElementById("email");
 let button = document.querySelector("button")
 button.onclick=function(){check(event)}
 button.addEventListener("click", function(event){event.preventDefault()})
 function check(){
-    if (set == ""){
+    if (set.value == ""){
         document.querySelector(".newsletter .message").textContent ="Please enter a valid email address";      
         
        
     }
     else{
-    alert("check")
-    document.querySelector(".newsletter .message").textContent ="Thank you! Your email address" + set +"has been added to our mailing listings";
+    document.querySelector(".newsletter .message").textContent ="Thank you! Your email address " + set.value +" has been added to our mailing listings";
         
 }
 }
