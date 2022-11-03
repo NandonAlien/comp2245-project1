05d528705dc39bc25538757bc618b463a37db93f
Nandon <nandonarts@gmail.com>
2022-11-02 20:38:15 -0400

    Complete Style Sheet portion and Adding parts to the script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-/* Add your JavaScript to this file */
+
+
+window.addEventListener('DOMContentLoaded',(event) =>{
+
+
+
+let set = document.querySelector("input #email");
+let button = document.querySelector("button")
+button.onclick=function(){check(event)}
+button.addEventListener("click", function(event){event.preventDefault()})
+function check(){
+    if (set == ""){
+        document.querySelector(".newsletter .message").textContent ="Please enter a valid email address";      
+        
+       
+    }
+    else{
+    alert("check")
+    document.querySelector(".newsletter .message").textContent ="Thank you! Your email address" + set +"has been added to our mailing listings";
+        
+}
+}
+
+});
